bfa333bb0f158dedbcaf048f48633c200c7e6507
Tatyana Shashkova <tatyana.shashkova@inworld.ai>
2023-08-17 19:53:53 -0700

    feat: Add scene name pattern validation

--- src/clients/inworld.client.ts
+++ src/clients/inworld.client.ts
@@ -1,5 +1,5 @@
 import { CapabilitiesRequest } from '../../proto/world-engine.pb';
-import { GRPC_HOSTNAME } from '../common/constants';
+import { GRPC_HOSTNAME, SCENE_PATTERN } from '../common/constants';
 import {
   Awaitable,
   Capabilities,
@@ -247,6 +247,10 @@ export class InworldClient<
   private validate() {
     if (!this.scene) {
       throw Error('Scene name is required');
+    }
+
+    if (!SCENE_PATTERN.test(this.scene)) {
+      throw Error('Scene name has wrong format');
     }
 
     const { audioPlayback } = this.config;
